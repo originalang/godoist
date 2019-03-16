3f0e8e3f8285c56df7b2ea96702a49ee83c4e2e7
originalang <angeorge200@gmail.com>
2019-03-16 01:03:56 -0400

    Added struct to hold task information

--- structures.go
+++ structures.go
@@ -6,3 +6,22 @@ type Project struct {
 	Indent int    `json:"indent"`
 	Order  int    `json:"order"`
 }
+
+type Task struct {
+	Id        int     `json:"id"`
+	Indent    int     `json:"indent"`
+	Order     int     `json:"order"`
+	Priority  int     `json:"priority"`
+	ProjectId int     `json:"project_id"`
+	Completed bool    `json:"completed"`
+	Content   string  `json:"content"`
+	Due       DueInfo `json:"due"`
+}
+
+type DueInfo struct {
+	Date       string `json:"date"`
+	Recurring  bool   `json:"recurring"`
+	DateTime   string `json:"datetime"`
+	StringDate string `json:"string"`
+	Timezone   string `json:"timezone"`
+}
