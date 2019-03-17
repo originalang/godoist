4538187a8b1a671ba0f50ce005436208990897e1
originalang <angeorge200@gmail.com>
2019-03-17 15:08:18 -0400

    New function to submit a request and encode the request body

--- client.go
+++ client.go
@@ -1,13 +1,38 @@
 package togoist
 
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+const endpoint string = "https://todoist.com/api/v7/sync"
+
 type Client struct {
-	Token     string
-	SyncToken string
+	HTTPClient *http.Client
+	Token      string
+	SyncToken  string
 }
 
 func NewClient(token string) *Client {
-	return &Client {
-		Token: token
-		SyncToken: "*"
+	return &Client{
+		HTTPClient: &http.Client{},
+		Token:      token,
+		SyncToken:  "*",
 	}
+}
+
+func (c *Client) NewRequest(method, urlPath string) *http.Response {
+
+	body := c.encodeBody()
+
+	req, _ := http.NewRequest(method, endpoint, body)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	resp, _ := c.HTTPClient.Do(req)
+	return resp
+}
+
+func (c *Client) encodeBody() *strings.Reader {
+	return strings.NewReader(fmt.Sprintf(`token=%s&sync_token=%s&resource_types=["projects"]`, c.Token, c.SyncToken))
 }
