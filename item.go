db44a8f5b89b20c9998d7dbea4081d73671cde94
originalang <angeorge200@gmail.com>
2019-04-04 20:51:05 -0400

    Alphabetized the elements in the struct

--- item.go
+++ item.go
@@ -3,14 +3,11 @@ package togoist
 type Item struct {
 	AllDay            bool   `json:"all_day"`
 	AssignedBy        int64  `json:"assigned_by_uid"`
-	DateCompleted     string `json:"date_completed"`
-	ParentId          int64  `json:"parent_id"`
-	ResponsibleUserId int64  `json:"responsible_uid"`
-	SyncId            string `json:"sync_id"`
 	Checked           bool   `json:"checked"`
 	Collapsed         int    `json:"collapsed"`
 	Content           string `json:"content"`
 	DateAdded         string `json:"date_added"`
+	DateCompleted     string `json:"date_completed"`
 	DateString        string `json:"date_string"`
 	DayOrder          int    `json:"day_order"`
 	DueDate           string `json:"due_date_utc"`
@@ -20,7 +17,10 @@ type Item struct {
 	IsArchived        bool   `json:"is_archived"`
 	IsDeleted         bool   `json:"is_deleted"`
 	ItemOrder         int    `json:"item_order"`
+	ParentId          int64  `json:"parent_id"`
 	Priority          int    `json:"priority"`
 	ProjectId         int64  `json:"project_id"`
+	ResponsibleUserId int64  `json:"responsible_uid"`
+	SyncId            string `json:"sync_id"`
 	UserId            int64  `json:"user_id"`
 }
