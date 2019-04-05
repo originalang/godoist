22e2934762c3736b98c93acca439e4595a7af2d4
originalang <angeorge200@gmail.com>
2019-04-05 00:47:54 -0400

    Added a function to convert a project to a map. This will be used to pass arguments to commands

--- helper.go
+++ helper.go
@@ -14,3 +14,16 @@ func decodeResponse(resp *http.Response) Response {
 
 	return decoded
 }
+
+func projectToMap(p *Project) map[string]interface{} {
+	proj, _ := json.Marshal(p)
+
+	var newMap map[string]interface{}
+	json.Unmarshal(proj, &newMap)
+
+	// the unmarshaling process does not parse the id field correctly
+	// this is done to preserve the correct id for the project
+	newMap["id"] = p.Id
+
+	return newMap
+}
