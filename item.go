1d79dcb553285e635f592bee9879a8a573c7ed9e
originalang <angeorge200@gmail.com>
2019-04-20 17:23:16 -0400

    Change multiple bool fields to int because the json was not being unmarshalled correctly into the struct

--- item.go
+++ item.go
@@ -3,7 +3,7 @@ package togoist
 type Item struct {
 	AllDay            bool   `json:"all_day"`
 	AssignedBy        int64  `json:"assigned_by_uid"`
-	Checked           bool   `json:"checked"`
+	Checked           int    `json:"checked"`
 	Collapsed         int    `json:"collapsed"`
 	Content           string `json:"content"`
 	DateAdded         string `json:"date_added"`
@@ -12,10 +12,10 @@ type Item struct {
 	DayOrder          int    `json:"day_order"`
 	DueDate           string `json:"due_date_utc"`
 	Id                int64  `json:"id"`
-	InHistory         bool   `json:"in_history"`
+	InHistory         int    `json:"in_history"`
 	Indent            int    `json:"indent"`
-	IsArchived        bool   `json:"is_archived"`
-	IsDeleted         bool   `json:"is_deleted"`
+	IsArchived        int    `json:"is_archived"`
+	IsDeleted         int    `json:"is_deleted"`
 	ItemOrder         int    `json:"item_order"`
 	ParentId          int64  `json:"parent_id"`
 	Priority          int    `json:"priority"`
